2c9672fb8a1d480a4826f79f92d56ffcf3887c8d
Alyxia Sother <alyxia@riseup.net>
2024-06-12 16:53:03 +0200

    [headers/*] Use craftcms-typedefs package

--- buildchain/src/js/headers/garnish.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Garnish {
-  interface Base {
-    readonly classId: number;
-  }
-  interface BaseConstructor extends Class<Base, object> {
-    new(): Base;
-  }
-  let Base: BaseConstructor;
-}
